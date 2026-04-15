791cd19155fbafdf8d7f6193260208709df424a8
cshan1021 <zulu1021@nate.com>
2026-04-15 17:05:48 +0900

    commit

--- python_routes.py
+++ python_routes.py
@@ -43,52 +43,48 @@ async def analyze_images(
         raise HTTPException(status_code=400, detail="이미지 파일이 전송되지 않았습니다.")
     
     results = []
-    try:
-        # 이미지 변환
-        base64_images = []
-        for file in files:
-            if not file.content_type.startswith("image/"):
-                continue
-            bytes_image = await file.read()
-            bytes_image = PythonUtil.bytes_resize(bytes_image, 640)
-            base64_images.append(PythonUtil.bytes_to_base64(bytes_image))
-        
-        # 이미지 확인
-        if len(base64_images) == 0:
-            raise HTTPException(status_code=400, detail="읽을 수 있는 이미지 파일이 없습니다.")
-        
-        # 이미지 분석
-        model_map = {
-            "deepseek": model_deepseek,
-            "gemma": model_gemma,
-            "internvl": model_internvl,
-            "llama": model_llama,
-            "llava": model_llava,
-            "qwen": model_qwen,
-        }
-        model_get = model_map.get(model, model_gemma)
-        # 여러개 한번에
-        idx = 0
+    # 이미지 변환
+    base64_images = []
+    for file in files:
+        if not file.content_type.startswith("image/"):
+            continue
+        bytes_image = await file.read()
+        bytes_image = PythonUtil.bytes_resize(bytes_image, 640)
+        base64_images.append(PythonUtil.bytes_to_base64(bytes_image))
+    
+    # 이미지 확인
+    if len(base64_images) == 0:
+        raise HTTPException(status_code=400, detail="읽을 수 있는 이미지 파일이 없습니다.")
+    
+    # 이미지 분석
+    model_map = {
+        "deepseek": model_deepseek,
+        "gemma": model_gemma,
+        "internvl": model_internvl,
+        "llama": model_llama,
+        "llava": model_llava,
+        "qwen": model_qwen,
+    }
+    model_get = model_map.get(model, model_gemma)
+    # 여러개 한번에
+    idx = 0
+    logging.info(f"분석 시작: {idx}")
+    result = model_get(base64_images)
+    logging.info(f"분석 종료: {idx}")
+    result["idx"] = idx
+    results.append(result)
+    # 메모리 정리
+    gc.collect()
+    # 하나씩 여러번
+    '''
+    for idx, base64_image in enumerate(base64_images):
         logging.info(f"분석 시작: {idx}")
-        result = model_get(base64_images)
+        result = model_get([base64_image])
         logging.info(f"분석 종료: {idx}")
         result["idx"] = idx
         results.append(result)
         # 메모리 정리
         gc.collect()
-        # 하나씩 여러번
-        '''
-        for idx, base64_image in enumerate(base64_images):
-            logging.info(f"분석 시작: {idx}")
-            result = model_get([base64_image])
-            logging.info(f"분석 종료: {idx}")
-            result["idx"] = idx
-            results.append(result)
-            # 메모리 정리
-            gc.collect()
-        '''
-    except Exception as e:
-        print(f"[Server Error] {e}")
-        raise HTTPException(status_code=500, detail=str(e))
-
+    '''
+    
     return JSONResponse(content={"status": "success", "data": results})
